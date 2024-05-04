83e30e405faf3d617987cd5106b4134c0ac4a73e
Giang Minh <giangndm@ows.vn>
2024-05-04 09:36:16 +0700

    added mock version for protobuf

---
+++ generate_protobuf.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PROTO_SRC=./lib/protobuf
+PROTO_DIR=./lib/generated/protobuf
+
+protoc -I=${PROTO_SRC} \
+    --plugin=node_modules/ts-proto/protoc-gen-ts_proto \
+    --ts_proto_out=outputClientImpl=grpc-web:${PROTO_DIR} \
+    ${PROTO_SRC}/*.proto
